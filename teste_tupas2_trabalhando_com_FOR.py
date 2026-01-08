4161aa940e35cc930f3ad874971575d12c576cab
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-08 01:58:29 -0300

    Utilizando Tuplas e o FOR.

---
+++ teste_tupas2_trabalhando_com_FOR.py
@@ -0,0 +1,7 @@
+# Tuplas são imutáveis
+lanche = ('Hambúrguer', 'Suco', 'Pizza','Pudim','Batata Frita', 'Açai')
+
+# atribuindo um FOR
+
+for comida in lanche:
+    print(f'Vou comprar na Lanchonete {comida}')
