23b8bf7a144db3abd430ed00a97dd0091b630e6d
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-08 02:32:48 -0300

    utilizando Sorted para organizar os itens da tupla, os mesmo são organizados por ordem alfabética, sem alterar os seus respctivos índices ou os itens pertecentes a tupla, pelo fato da tupla ser imutável.

---
+++ Aula_16_tuplas/teste_tuplas6_com_sorted.py
@@ -0,0 +1,11 @@
+# Tuplas são imutáveis
+# Sorted -> utilizado para organizar por ordem alfabética os item, se alterar
+# a tuplas, pois Tuplas são imutáveis.
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza','Pudim','Batata Frita', 'Açai')
+print(lanche)
+
+# Usando "Sorted" na segunda impressão em tela, onde houve organização alfabética
+# sem alterar os itens da Tupla e sem alterar os índices
+print(sorted(lanche))
+
