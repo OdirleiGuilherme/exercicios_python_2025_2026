c741acc79cb7098e0ef69d5bd4eb6fd968e58759
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-08 02:05:00 -0300

    Utilizando Tuplas com FOR, RANGE e LEN.

---
+++ teste_tuplas3_trabalhando_com_for_range_len.py
@@ -0,0 +1,8 @@
+# Tuplas são imutáveis
+lanche = ('Hambúrguer', 'Suco', 'Pizza','Pudim','Batata Frita', 'Açai')
+
+# Utilizando FOR, RANGE E LEN.
+
+for cont in range(0, len(lanche)):
+    print(lanche[cont])
+print('Gastei uma grana!')
