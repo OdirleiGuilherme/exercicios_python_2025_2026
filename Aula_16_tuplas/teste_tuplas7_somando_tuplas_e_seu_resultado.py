081b02ab46869e42136ac96f2813d074d9d876d1
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-09 02:23:55 -0300

    resultado somando uma duas tuplas.

---
+++ Aula_16_tuplas/teste_tuplas7_somando_tuplas_e_seu_resultado.py
@@ -0,0 +1,10 @@
+# trabalahando com Tuplas
+# Tuplas são imutáveis
+# Aqui veremos como o programa se comporta com duas tuplas A e B e Somando elas com a variável C
+
+a = (2,4,6,8,9)
+b = (1,3,5,7,9,10)
+c = a + b
+print(c)
+
+# observe que não houve a soma das tuplas, mas a junção das mesma respeitando a sequência da tupla A de depois B
