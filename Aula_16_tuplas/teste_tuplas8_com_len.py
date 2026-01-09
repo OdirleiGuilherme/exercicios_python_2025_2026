07a25f82837514cda56f1fd89241354c0ffa9cee
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-09 02:24:34 -0300

    usando o len para ver quantos itens tem em uma ou duas tuplas somadas.

---
+++ Aula_16_tuplas/teste_tuplas8_com_len.py
@@ -0,0 +1,10 @@
+# trabalahando com Tuplas
+# Tuplas são imutáveis
+# Avamos verificar quantos números temos no total de A e B com o LEN, lembrando que estou verificando com a junção
+# de A + B em C
+
+a = (2,4,6,8,9)
+b = (1,3,5,7,9,10)
+c = a + b
+print(len(c))
+
