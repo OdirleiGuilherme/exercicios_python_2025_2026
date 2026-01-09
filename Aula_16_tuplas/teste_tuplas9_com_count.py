58ca321b1e0e7a77659125a9879e119332f2a193
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-09 02:25:35 -0300

    utilizando .count para contar nas tuplas.

---
+++ Aula_16_tuplas/teste_tuplas9_com_count.py
@@ -0,0 +1,9 @@
+# trabalahando com Tuplas
+# Tuplas são imutáveis
+# Aqui veremos como o programa se comporta com duas tuplas A e B e vamos fazer uma contagem de quantas vez o 9 se repete
+
+a = (2,4,6,8,9)
+b = (1,3,5,7,9,10)
+c = a + b
+print(c.count(9))
+# O count serve para fazer a contagem!
