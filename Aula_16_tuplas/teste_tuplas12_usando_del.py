626bcecfa57cd26a3372e2b7f8dbc0dc035a5dc7
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-01-09 02:27:25 -0300

    utilizando o del para apagar uma variável em tupla.

---
+++ Aula_16_tuplas/teste_tuplas12_usando_del.py
@@ -0,0 +1,6 @@
+pessoa = ('Guilherme', 45, 'M', 85.50)
+#del(pessoa)
+print(pessoa)
+
+# Para não esquecer as tuplas são imutaveis, mas podemos deletar utilizando o comando 'DEL', uma vez fazendo isso
+# não fica um único resquício da variável
